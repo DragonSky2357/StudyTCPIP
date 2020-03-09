7db40e753a4263b7f5fbcfe4f1274e0107adcc8f
username <qkrdydals327@gmail.com>
2020-03-09 19:36:00 +0900

    2020-03-09

---
+++ Client/Client/Source/WindowsConsole/2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client.Source.WindowsConsole._2 {
+    class Program {
+        static void Main(string[] args) {
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1994);
+            sck.Connect(endPoint);
+
+            Console.WriteLine("Enter Message: ");
+            string msg = Console.ReadLine();
+            byte[] msgBuffer = Encoding.UTF8.GetBytes(msg);
+            sck.Send(msgBuffer, 0, msgBuffer.Length, SocketFlags.None);
+
+            byte[] buffer = new byte[255];
+            int rec = sck.Receive(buffer, 0, buffer.Length, SocketFlags.None);
+
+            Array.Resize(ref buffer, rec);
+
+            Console.WriteLine("Recevied: {0}", Encoding.UTF8.GetString(buffer));
+
+            Console.Read();
+        }
+    }
+}
---
+++ Server/Server/Source/WindowsConsole/2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server.Source.WindowsConsole._2 {
+    class Program {
+        static void Main(string[] args) {
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            sck.Bind(new IPEndPoint(0, 1994));
+            sck.Listen(0);
+
+            Socket acc = sck.Accept();
+
+            byte[] buffer = Encoding.UTF8.GetBytes("Hello Client");
+            acc.Send(buffer, 0, buffer.Length, SocketFlags.None);
+
+            buffer = new byte[acc.SendBufferSize];
+            int rec = acc.Receive(buffer, 0, buffer.Length, SocketFlags.None);
+
+            Array.Resize(ref buffer, rec);
+
+            Console.WriteLine("Received: {0}", Encoding.UTF8.GetString(buffer));
+
+            sck.Close();
+            acc.Close();
+
+            Console.Read();
+        }
+        
+    }
+}
